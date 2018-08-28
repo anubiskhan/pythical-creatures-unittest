049e1e3a1e22d13c2a224f1bac3a1db398dc8d8e
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-08-27 20:13:49 -0600

    Add completed pirate test for unittest

---
+++ test_pirate.py
@@ -0,0 +1,49 @@
+from creatures.pirate import Pirate
+import unittest
+
+class TestPirate(unittest.TestCase):
+    def test_has_name(self):
+        pirate = Pirate('Jack')
+        self.assertEqual(pirate.name, 'Jack')
+
+    def test_can_have_different_name(self):
+        pirate = Pirate('Blackbeard')
+        self.assertEqual(pirate.name, 'Blackbeard')
+
+    def test_is_a_scallywag_by_default(self):
+        pirate = Pirate('Jack')
+        self.assertEqual(pirate.job, 'Scallywag')
+
+    def test_is_not_always_a_scallywag(self):
+        pirate = Pirate('Jack', 'Cook')
+        self.assertEqual(pirate.job, 'Cook')
+
+    def test_isnt_cursed_by_default(self):
+        pirate = Pirate('Jack')
+        self.assertFalse(pirate.is_cursed())
+
+    def test_becomes_cursed_after_enough_heinous_acts(self):
+        pirate = Pirate('Jack')
+        self.assertFalse(pirate.is_cursed())
+        pirate.commit_heinous_act()
+        self.assertFalse(pirate.is_cursed())
+        pirate.commit_heinous_act()
+        self.assertFalse(pirate.is_cursed())
+        pirate.commit_heinous_act()
+        self.assertTrue(pirate.is_cursed())
+
+    def test_a_pirate_can_rob_ships(self):
+        # create a pirate
+        # check that a pirate has a rob_ship method
+
+    def test_a_pirate_has_booty(self):
+        # create a pirate
+        # pirate starts with 0 booty
+
+    def test_a_pirate_gets_100_booty_for_robbon(self):
+        # create a pirate
+        # pirate robs ship
+        # check pirate has 100 booty
+
+if __name__ == '__main__':
+    unittest.main()
