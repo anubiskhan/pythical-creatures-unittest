09d5a2c171bc8b8d94f364e2b84af406bb1f85d7
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-08-27 22:55:41 -0600

    Add skips to wizard tests

--- test_wizard.py
+++ test_wizard.py
@@ -6,34 +6,42 @@ class TestWizard(unittest.TestCase):
         wizard = Wizard('Eric')
         self.assertEqual(wizard.name, 'Eric')
 
+    @unittest.skip('Skipped Test')
     def test_can_have_different_name(self):
         wizard = Wizard('Alex')
         self.assertEqual(wizard.name, 'Alex')
 
+    @unittest.skip('Skipped Test')
     def test_is_bearded_by_default(self):
         wizard = Wizard('Ben')
         self.assertTrue(wizard.is_bearded())
 
+    @unittest.skip('Skipped Test')
     def test_is_not_always_bearded(self):
         wizard = Wizard('Valerie', 'bearded': False)
         self.assertFalse(wizard.is_bearded())
 
+    @unittest.skip('Skipped Test')
     def test_has_root_powers(self):
         wizard = Wizard('Sarah', 'bearded': False)
         self.assertEqual(wizard.incantation('chown ~/bin'), 'sudo chown ~/bin')
 
+    @unittest.skip('Skipped Test')
     def test_has_lots_of_root_powers(self):
         wizard = Wizard('Rob', 'bearded': False)
         self.assertEqual(wizard.incantation('rm -rf /home/mirandax'), 'sudo rm -rf /home/mirandax')
 
+    @unittest.skip('Skipped Test')
     def test_starts_rested(self):
         # create wizard
         # .is_rested() returns true
 
+    @unittest.skip('Skipped Test')
     def test_can_cast_spells(self):
         # create wizard
         # .cast() returns 'MAGIC MISSILE!'
 
+    @unittest.skip('Skipped Test')
     def test_gets_tired_after_casting_three_spells(self):
         # create wizard
         # casts spell twice
